e6ace8c39d72a5cbb2b4792a584398ec5ae30984
BuSheeZy <busheezy@gmail.com>
2022-07-18 13:11:17 -0700

    whoops

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,5 @@ if [ "true" == "$GENERATE_API" ]; then
   sleep 10
   curl ${TYPES_URL} --output /vite-sharex/spec.json
   pnpm openapi --input /vite-sharex/spec.json --output /vite-sharex/src/api
+  pnpm build
 fi
-
-pnpm build
